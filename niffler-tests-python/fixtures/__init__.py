3b8d1290657cd2b34100c96f991222aad893557d
AlexLawKas <AlexLawKas@yandex.ru>
2026-02-15 23:09:11 +0400

    fix

--- niffler-tests-python/fixtures/__init__.py
+++ niffler-tests-python/fixtures/__init__.py
@@ -7,19 +7,19 @@ if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
 
-from .authorization import login_user, login_page
-from .person import person_generator, user_data
-from .spendings import (spends_client, currency, create_category,
+from fixtures.authorization import login_user, login_page
+from fixtures.person import person_generator, user_data
+from fixtures.spendings import (spends_client, currency, create_category,
                         create_second_category, category_value,
                         add_spending_page, spend_data_for_add,
                         add_spend, spend_data_for_edit)
-from .profile import open_profile_page, profile_page
-from .alerts import archive_category_alert, logout_alert
-from .header_element import header_element
-from .main_page import main_page
-from .navbar import navbar_element
-from .registration import registration_page
-from .soap_fixtures import (api_session, api_wookie_session,
+from fixtures.profile import open_profile_page, profile_page
+from fixtures.alerts import archive_category_alert, logout_alert
+from fixtures.header_element import header_element
+from fixtures.main_page import main_page
+from fixtures.navbar import navbar_element
+from fixtures.registration import registration_page
+from fixtures.soap_fixtures import (api_session, api_wookie_session,
                             api_destruction_session, api_module_session,
                             soap_session)
 
