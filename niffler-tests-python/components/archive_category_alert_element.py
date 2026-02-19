b817c589bf81d918f4ff0ee3fd37f39a44a6ff4c
AlexLawKas <AlexLawKas@yandex.ru>
2026-02-19 23:34:39 +0400

    fix

--- niffler-tests-python/components/archive_category_alert_element.py
+++ niffler-tests-python/components/archive_category_alert_element.py
@@ -3,7 +3,8 @@ from page_factory.button import Button
 
 class ArchiveCategoryAlertElement:
     def __init__(self, page):
-        self.archive_button = Button(page, locator="Archive", name="Archive", strategy="text")
+        self.archive_button = Button(page, locator=".MuiDialogActions-root>.MuiButtonBase-root:nth-child(2)",
+                                     name="Archive")
 
     def click_archive_button(self):
         self.archive_button.click_last()
